6c13a1b9e73129c4f1e53ecbea107792c9acf7c9
Eduardo Argou <ed.argou@gmail.com>
2023-08-30 16:15:42 -0300

    fix(): retrieves products by id instead of loading entire collection (#1)

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -93,9 +93,6 @@ class GatewayService(object):
         # raise``OrderNotFound``
         order = self.orders_rpc.get_order(order_id)
 
-        # Retrieve all products from the products service
-        product_map = {prod['id']: prod for prod in self.products_rpc.list()}
-
         # get the configured image root
         image_root = config['PRODUCT_IMAGE_ROOT']
 
@@ -103,7 +100,9 @@ class GatewayService(object):
         for item in order['order_details']:
             product_id = item['product_id']
 
-            item['product'] = product_map[product_id]
+            # Get item details
+            item['product'] = self.products_rpc.get(product_id)
+
             # Construct an image url.
             item['image'] = '{}/{}.jpg'.format(image_root, product_id)
 
@@ -156,13 +155,10 @@ class GatewayService(object):
         return Response(json.dumps({'id': id_}), mimetype='application/json')
 
     def _create_order(self, order_data):
-        # check order product ids are valid
-        valid_product_ids = {prod['id'] for prod in self.products_rpc.list()}
-        for item in order_data['order_details']:
-            if item['product_id'] not in valid_product_ids:
-                raise ProductNotFound(
-                    "Product Id {}".format(item['product_id'])
-                )
+        for item in order_data['order_details']: 
+            # Check order product id is valid
+            # Note - this may raise `ProductNotFound`
+            self.products_rpc.get(item['product_id'])
 
         # Call orders-service to create the order.
         # Dump the data through the schema to ensure the values are serialized
